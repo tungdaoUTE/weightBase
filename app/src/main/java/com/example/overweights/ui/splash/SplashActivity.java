c0f39a011bd8b21720054691343694dae3562db9
đào ngọc tùng <39922540+tungdaoUTE@users.noreply.github.com>
2021-02-05 12:27:40 +0700

    check first start

--- app/src/main/java/com/example/overweights/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/overweights/ui/splash/SplashActivity.java
@@ -3,11 +3,18 @@ package com.example.overweights.ui.splash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.overweights.databinding.ActivitySplashBinding;
 import com.example.overweights.ui.intro.IntroActivity;
+import com.example.overweights.ui.login.LoginActivity;
+import com.example.overweights.utils.SharedPref;
+
+import static java.lang.Thread.sleep;
 
 public class SplashActivity extends AppCompatActivity {
     ActivitySplashBinding binding;
@@ -18,19 +25,26 @@ public class SplashActivity extends AppCompatActivity {
         binding = ActivitySplashBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        final Thread thread = new Thread() {
+        final Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
+                SharedPreferences getpre = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+                boolean isfirtstart = getpre.getBoolean("firststart", true);
                 try {
-                    sleep(3000);
-
+                    sleep(2000);
+                    if (isfirtstart) {
+                        startActivity(new Intent(SplashActivity.this, IntroActivity.class));
+                        SharedPreferences.Editor e = getpre.edit();
+                        e.putBoolean("firststart", false);
+                        e.apply();
+                    } else {
+                        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    }
                 } catch (Exception ex) {
                     ex.printStackTrace();
-                } finally {
-                    startActivity(new Intent(SplashActivity.this, IntroActivity.class));
                 }
             }
-        };
+        });
         thread.start();
     }
 }
