af3cf285da016f1bdbdc4e63af49d99deddbf015
đào ngọc tùng <39922540+tungdaoUTE@users.noreply.github.com>
2021-02-20 00:07:01 +0700

    discover step1

---
+++ app/src/main/java/com/example/overweights/ui/MainPage/DiscoverFragment.java
@@ -0,0 +1,11 @@
+package com.example.overweights.ui.MainPage;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.overweights.databinding.FragmentPlanBinding;
+
+public class DiscoverFragment extends Fragment {
+
+    public DiscoverFragment() {
+    }
+}
